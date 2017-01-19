5f7d7413293055f7224abb736a8e48afdc0142a2
Egor Biriukov <birykovegor@gmail.com>
2017-01-19 11:16:22 +0300

    Formatting and small fixes

--- hw2/2.sql
+++ hw2/2.sql
@@ -8,6 +8,7 @@ CREATE TABLE Conference(
     name TEXT PRIMARY KEY,
     description TEXT
 );
+
 CREATE TABLE Venue(
     id INTEGER PRIMARY KEY,
     name TEXT,
@@ -17,6 +18,7 @@ CREATE TABLE Venue(
     lon NUMERIC(8,5),
     UNIQUE(lat, lon)
 );
+
 CREATE TABLE PaperSubmission(
   id INTEGER PRIMARY KEY,
   conference TEXT,
@@ -31,10 +33,3 @@ CREATE TABLE PaperSubmission(
   UNIQUE(title, isbn),
   UNIQUE(page, isbn)
 );
-
-INSERT INTO Conference(name, description) VALUES ('SIGMOD', 'The annual ACM SIGMOD/PODS conference is a leading international forum for database researchers, practitioners, developers, and user$
-INSERT INTO Conference(name, description) VALUES ('VLDB', 'VLDB is a premier annual international forum for data management and database researchers, vendors, practitioners, application develop$
-INSERT INTO Venue(id, name, city, country, lat, lon) VALUES (1, 'Moscone Center', 'San Francisco', 'US', 37.78417, -122.40155);
-INSERT INTO Venue(id, name, city, country, lat, lon) VALUES (2, 'Davos Congress Centre', 'Davos', 'CH', 46.7939, 9.82091);
-INSERT INTO PaperSubmission(id, conference, year, title, isbn, page, venue_id) VALUES
-  (1, 'SIGMOD', 2016, 'Creating yellow squared submarines from iron', NULL, NULL, 1);
