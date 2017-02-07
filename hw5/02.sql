35c142c66238d9e25866f84be3fdedafb4ae0056
Egor Biriukov <birykovegor@gmail.com>
2017-02-07 16:47:14 +0300

    Solve #2

--- hw5/02.sql
+++ hw5/02.sql
@@ -5,25 +5,25 @@ WeeklyCompanyGrowth AS (
                                                                      ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) as growth
   FROM StockQuotes
 ),
-Index AS (
+WeeklyIndex AS (
   SELECT week, AVG(growth) as index
   FROM WeeklyCompanyGrowth
   GROUP BY week
 ),
 CompanySuccess AS (
   SELECT wcg.company, wcg.week, CASE WHEN wcg.growth > i.index THEN 1 ELSE 0 END as was_success
-  FROM WeeklyCompanyGrowth wcg JOIN Index i ON wcg.week = i.week
+  FROM WeeklyCompanyGrowth wcg JOIN WeeklyIndex i ON wcg.week = i.week
   ORDER BY company, week ASC
 ),
 CompanyPeriods AS (
   SELECT company, SUM(was_success) OVER (PARTITION BY company
                                          ORDER BY week ASC
-                                         ROWS BETWEEN 3 PRECEDING AND CURRENT ROW) as success_rate
+                                         ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) as success_rate
   FROM CompanySuccess
 )
-SELECT company, COUNT(*) as success_periods
+SELECT company, COUNT(*)
 FROM CompanyPeriods
 WHERE success_rate = 3
 GROUP BY company
 HAVING COUNT(*) > 0
-ORDER BY success_periods, company;
+ORDER BY COUNT(*), company ASC;
